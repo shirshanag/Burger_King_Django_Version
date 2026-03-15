8d64530433b59e0bbe06276f0e22a6e8f17876cf
shirshanag <15shirsha@gmail.com>
2026-03-16 00:40:21 +0530

    Building backend of booking.html

--- foodie/app1/views.py
+++ foodie/app1/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(requests):
-    return render(requests,'index.html')
-def about(requests):
-    return render(requests,'about.html')
-def blog(requests):
-    return render (requests,'blog.html')
-def booking(requests):
-    return render(requests,'booking.html')
-def contact(requests):
-    return render(requests,'contact.html')
-def feature(requests):
-    return render(requests,"feature.html")
-def menu(requests):
-    return render(requests,"menu.html")
-def single(requests):
-    return render (requests,"single.html")
-def team(requests):
-    return render(requests,"team.html")
+def index(request):
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+def blog(request):
+    return render (request,'blog.html')
+def booking(request):
+    if request.method=="POST":
+        a1=request.POST['name']
+        a2=request.POST['email']
+        a3=request.POST['mobile']
+        a4=request.POST['date']
+        a5=request.POST['time']
+        a6=request.POST['guests']
+        from .models import Book_data
+        data=Book_data(name=a1,email=a2,mobile=a3,date=a4,time=a5,guests=a6)
+        data.save()
+
+    return render(request,'booking.html')
+def contact(request):
+    return render(request,'contact.html')
+def feature(request):
+    return render(request,"feature.html")
+def menu(request):
+    return render(request,"menu.html")
+def single(request):
+    return render (request,"single.html")
+def team(request):
+    return render(request,"team.html")
